b982751bb1673cc838968ecd6f218e03cc1b0a10
armando20mil <ardek_romjuz@outlook.com>
2021-12-21 14:53:39 -0600

    Updates

---
+++ precache-manifest.0855c34db74dad5dca6d353aa2d4d138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1123d1bb7a88dbd9aa75b61159d40644",
+    "url": "/react-app-hangman-basic/index.html"
+  },
+  {
+    "revision": "8e6fa3b6380ad2828d19",
+    "url": "/react-app-hangman-basic/static/css/main.922c2fae.chunk.css"
+  },
+  {
+    "revision": "71dcf383efe41087b7cc",
+    "url": "/react-app-hangman-basic/static/js/2.691b744d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-hangman-basic/static/js/2.691b744d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6fa3b6380ad2828d19",
+    "url": "/react-app-hangman-basic/static/js/main.784614be.chunk.js"
+  },
+  {
+    "revision": "590289fd41804a487a64",
+    "url": "/react-app-hangman-basic/static/js/runtime-main.04316e87.js"
+  }
+]);
